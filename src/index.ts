db732a1fd3e42cdbbc81bfc61a2984e71e9e4654
Victor Prado <prado.victor1005@gmail.com>
2022-11-15 21:56:13 -0300

    introduzindo mongodb no projeto e criando os models

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
 
-app.listen(3001, () => {
-  console.log('Server is running on http://localhost:3001');
-});
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+
+    app.listen(3001, () => {
+      console.log('Server is running on http://localhost:3001');
+    });
+  }
+  )
+  .catch(() => console.log('Erro ao conectar ao mongodb'));
+
+
